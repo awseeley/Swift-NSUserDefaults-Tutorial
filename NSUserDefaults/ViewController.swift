9e0a79b569ffd09c9eeb0a988b4850c7d833db81
Andrew Seeley <awseeley@gmail.com>
2016-01-05 19:12:43 +1100

    Initial
    NSUserDefaults

--- NSUserDefaults/ViewController.swift
+++ NSUserDefaults/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblData: UILabel!
+    
+    let userDefaults = NSUserDefaults.standardUserDefaults()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +22,22 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnSave(sender: AnyObject) {
+        let data = "Hello"
+        userDefaults.setObject(data, forKey: "keyHello")
+    }
+    
+    @IBAction func btnLoad(sender: AnyObject) {
+        
+        let data: String? = userDefaults.objectForKey("keyHello") as! String?
+        if(data != nil) {
+            lblData.text = data
+        } else {
+            lblData.text = "Nothing Saved"
+        }
+        
     }
 
 
